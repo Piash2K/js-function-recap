04192a3a52a1010e48b692bec287859079e69ede
PIASH <zmnobab1999@gmail.com>
2025-11-23 23:00:48 +0600

    recap function syntax

---
+++ function.js
@@ -0,0 +1,30 @@
+const number = 45;
+
+function fanOffKor() {
+  console.log("bosa theke uthe dara");
+  console.log("walk towards the switch");
+  console.log("click the switch to off of the Fan");
+}
+
+//call the function
+fanOffKor();
+
+//function name(){
+//
+// }
+function brushTeeth() {
+  console.log("pick up the brush");
+  console.log("add paste");
+  console.log("2 min gosho and gosho and fena felo");
+}
+
+//call the function
+brushTeeth();
+
+console.log("------------");
+console.log("------------");
+console.log("------------");
+console.log("------------");
+console.log("tom tom ta to");
+
+brushTeeth();
